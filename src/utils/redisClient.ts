c7f05af16e505fbe66172368ba3ea5530bc8f4c6
Vansh Dhawan <vanshdhawan2505@gmail.com>
2024-10-04 19:11:25 +0530

    Update

--- src/utils/redisClient.ts
+++ src/utils/redisClient.ts
@@ -1,9 +1,12 @@
 import {Redis} from 'ioredis'
-
+import dotenv from 'dotenv'
+dotenv.config();
+const REDIS_HOST = process.env.REDIS_HOST
+const REDIS_PORT = process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT, 10) : 6379
 // Creating a new Redis client instance with specified host and port
 const redisClient = new Redis({
-    host: 'localhost',
-    port: 6379,       
+    host: REDIS_HOST,
+    port: REDIS_PORT,       
 });
 
 export default redisClient
